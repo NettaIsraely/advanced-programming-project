9dc93d75a63dc152f6098914d656fd8d2cf247a1
yoav-ro <yoavro.pro@gmail.com>
2026-03-02 17:20:29 +0200

    fitting pep8 standards

--- src/tlvflow/domain/reports.py
+++ src/tlvflow/domain/reports.py
@@ -1,13 +1,19 @@
-from datetime import datetime
-from tlvflow.domain.enums import ReportStatus
 import re
+from datetime import datetime
 from uuid import uuid4
 
+from tlvflow.domain.enums import ReportStatus
+
+
 class VehicleReport:
-    def __init__(self, user_id: str, vehicle_id: str, 
-                 submission_time: datetime, image_url: str, 
-                 description: str):
-        
+    def __init__(
+        self,
+        user_id: str,
+        vehicle_id: str,
+        submission_time: datetime,
+        image_url: str,
+        description: str,
+    ):
         # Protected attributes
         self._report_id: str = uuid4().hex
         self._user_id: str = user_id
@@ -19,18 +25,26 @@ class VehicleReport:
         self.__description: str = description
         self.__status: ReportStatus = ReportStatus.SUBMITTED
 
-    def verify_damage(self):
-        mock_ai_validation_result=True # As we do not have the tools to review the photo and analyse a vehicle's damage, a mock of such a test's result
-        img_url_pattern = r'^https?://.*\.(?:png|jpg|jpeg|gif|bmp)$' # Review a valid image URL structure
-        if re.match(img_url_pattern, self.__image_url, re.IGNORECASE) is None or not mock_ai_validation_result:
-            self.__status=ReportStatus.REJECTED
+    def verify_damage(self) -> bool:
+        # As we do not have the tools to review the photo and analyze a vehicle's damage, a mock of such a test's result is used.
+        mock_ai_validation_result = True
+        
+        # Review for a valid image URL structure
+        img_url_pattern = r'^https?://.*\.(?:png|jpg|jpeg|gif|bmp)$'
+        
+        if (
+            re.match(img_url_pattern, self.__image_url, re.IGNORECASE) is None
+            or not mock_ai_validation_result
+        ):
+            self.__status = ReportStatus.REJECTED
             return False
-        self.__status=ReportStatus.VERIFIED
+            
+        self.__status = ReportStatus.VERIFIED
         return True
 
     def submit_report(self):
-        self.__status=ReportStatus.SUBMITTED
+        self.__status = ReportStatus.SUBMITTED
 
     def _create_maintenance_event(self):
-        #TODO - implement after creating maintance event class
+        # TODO: Implement after creating maintenance event class
         pass
